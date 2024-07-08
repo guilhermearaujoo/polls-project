66db7c0ae3295f4f155cb0bc0742e244d7bc8439
guilhermearaujoo <ghma.em1@gmail.com>
2024-07-08 15:28:41 -0300

    test: ensure RemoreAuthentication returns an Account on success

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,9 +1,16 @@
 import { faker } from '@faker-js/faker'
+import { type AccountModel } from '../models/account-model'
 import { type AuthenticationParams } from '../usecases/authentication'
 
 export const mockAuthentication = (): AuthenticationParams => {
   return {
     email: faker.internet.email(),
     password: faker.internet.password()
+  }
+}
+
+export const mockAccountModel = (): AccountModel => {
+  return {
+    accessToken: faker.string.uuid()
   }
 }
