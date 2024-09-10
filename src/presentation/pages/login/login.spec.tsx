b1289a4834e0331750757bd85a5947bccfd9b21c
guilhermearaujoo <ghma.em1@gmail.com>
2024-09-09 23:12:10 -0300

    test: ensure login presents loading on submit

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -94,4 +94,20 @@ describe('Login Component', () => {
     const submitButton = sut.getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(false)
   })
+
+  test('Should show loading spinner on submit', () => {
+    const { sut } = makeSut()
+
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: faker.internet.password() } })
+
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+
+    const submitButton = sut.getByTestId('submit')
+    fireEvent.click(submitButton)
+
+    const spiner = sut.getByTestId('spiner')
+    expect(spiner).toBeTruthy()
+  })
 })
