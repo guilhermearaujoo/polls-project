9a822f131fc1102421fc8f409b5c790f2c8ea858
guilhermearaujoo <ghma.em1@gmail.com>
2024-09-23 21:51:39 -0300

    feat: ensure EmailValidation returns falsy if field is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,19 @@
 import { InvalidFieldError } from '@/validation/errors'
+import { faker } from '@faker-js/faker'
 import { EmailValidation } from './email-validation'
+
+const makeSut = (): EmailValidation => new EmailValidation(faker.word.sample())
 
 describe('EmailValidator', () => {
   test('Should return error if email is invalid', () => {
-    const sut = new EmailValidation('email')
-    const error = sut.validate('')
+    const sut = makeSut()
+    const error = sut.validate(faker.word.sample())
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return FALSY if email is valid', () => {
+    const sut = makeSut()
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
