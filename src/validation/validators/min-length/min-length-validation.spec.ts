7d2a12be75b039feb5ee3ed86e6083ebd218b0d5
guilhermearaujoo <ghma.em1@gmail.com>
2024-10-24 16:37:25 -0300

    test: ensure MinLengthValidation returns error if value is invalid

---
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -0,0 +1,9 @@
+import { InvalidFieldError } from '@/validation/errors'
+import { MinLengthValidation } from './min-length-validation'
+
+describe('MinLengthValidation', () => {
+  test('Should return error if value is invalid', () => {
+    const sut = new MinLengthValidation('field', 5)
+    expect(sut.validate('123')).toEqual(new InvalidFieldError())
+  })
+})
